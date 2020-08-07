e9cb02f634a480f6f8272bd3e6a9a6e5f1a04af5
Yasmin <yasmin@plugindata.com.br>
2020-08-07 11:13:50 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixação07 { 
+    class Program {
+        static void Main(string[] args) {
+
+            int minutos;
+            double conta;
+
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100) { 
+                conta += (minutos - 100) * 2.0;
+        }
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+        }
+    }
+}
